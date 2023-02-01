e19df249c98149b766365d9eaa7fe8b6290e5c37
Timur <timur.shyngys0@mail.ru>
2023-02-01 21:35:56 +0600

    Upload tsis1

---
+++ tsis1/operators.py
@@ -0,0 +1,32 @@
+print(10+2)
+print(10-2)
+print(10*2)
+print(10/2)
+print(10%2)
+print(10**2)
+print(33//2)
+
+x=5
+x+=3
+print(x)
+
+x=5
+x%=3
+print(x)
+
+x = 5
+x//=3
+print(x)
+
+"""
+and 	Returns True if both statements are true	
+or	    Returns True if one of the statements is true	
+not 	Reverse the result, returns False if the result is true
+"""
+
+
+"""
+is  	Returns True if both variables are the same object	x is y	
+is not	Returns True if both variables are not the same object
+"""
+
